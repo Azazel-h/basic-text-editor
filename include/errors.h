6a61a5c971c2c43c8ba030c83af9fc95b8cbba42
Yasuhiro <yasuhiro.corp@gmail.com>
2024-07-05 20:01:44 +0300

    Basic GLFW + Vulcan cross-platform (Linux, Windows)

---
+++ include/errors.h
@@ -0,0 +1,12 @@
+//
+// Created by Yasuhiro on 05.07.2024.
+//
+
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#define OK 0
+#define GLFW_INIT_ERROR 1
+#define GLFW_WINDOW_CREATION_ERROR 2
+
+#endif //ERRORS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,53 @@
 //
 // Created by Yasuhiro on 05.07.2024.
 //
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#include "errors.h"
 #include <iostream>
+
+static void error_callback(int error, const char* description)
+{
+    std::cerr << "Error: " << description << std::endl;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main()
 {
-    std::cout << "Hello, World!";
-    return 0;
+    int rc = OK;
+    if (glfwInit())
+    {
+        glfwSetErrorCallback(error_callback);
+        GLFWwindow* window = glfwCreateWindow(640, 480, "TextEditor", nullptr, nullptr);
+        if (window)
+        {
+            glfwSetKeyCallback(window, key_callback);
+            glfwMakeContextCurrent(window);
+            while(!glfwWindowShouldClose(window))
+            {
+                glfwPollEvents();
+                glfwSwapBuffers(window);
+            }
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        }
+        else
+            rc = GLFW_WINDOW_CREATION_ERROR;
+    }
+    else
+        rc = GLFW_INIT_ERROR;
+
+    return rc;
 }
